0cff4f41a1d7ad9aab137136cb4dda0bdeb72ead
parkedhampster <jmentz@gmx.com>
2023-06-28 15:21:38 -0400

    update pkm browser now able to search by types type positioning is agnostic name search is case insensitive

--- app.py
+++ app.py
@@ -1,2 +1,75 @@
 import pandas as pd
 import streamlit as st
+
+st.set_page_config(
+        page_title="Fantasy Pokemon League",
+)
+
+st.sidebar.title("Pokemon Browser")
+
+pkm_dex = pd.read_csv('./data/pkm_base.tsv',sep='\t',index_col=0)
+gen_max = pd.read_csv('./data/gen_max.tsv',sep='\t')
+
+curr_gen = 4
+
+act_dex = pkm_dex[
+        pkm_dex.index <=
+        gen_max[
+            gen_max['gen'] == curr_gen
+            ]['max'].iloc[0]
+        ]
+
+typelist = [
+    '',
+    'Normal',
+    'Fire',
+    'Water',
+    'Grass',
+    'Electric',
+    'Ice',
+    'Fighting',
+    'Poison',
+    'Ground',
+    'Flying',
+    'Psychic',
+    'Bug',
+    'Rock',
+    'Ghost',
+    'Dark',
+    'Dragon',
+    'Steel',
+    'Fairy',
+]
+
+st.title("Fantasy Pokemon League")
+st.header("Pokemon Browser")
+
+pk_name_search = st.text_input("Pokemon Name")
+
+type_cols = st.columns(2)
+
+with type_cols[0]:
+    type1 = st.selectbox(
+        "Type 1",
+        typelist
+    )
+
+with type_cols[1]:
+    type2 = st.selectbox(
+        "Type 2",
+        typelist
+    )
+
+
+
+act_dex[
+    (act_dex['Name'].str.contains(pk_name_search,na=False,case=False)) &
+    (
+        (act_dex['Type 1'].str.contains(type1,na=False)) |
+        (act_dex['Type 2'].str.contains(type1,na=False))
+    ) &
+    (
+        (act_dex['Type 1'].str.contains(type2,na=False)) |
+        (act_dex['Type 2'].str.contains(type2,na=False))
+    )
+    ]
