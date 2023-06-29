ed6f88b22cb3a0e051a5f7c8b598f62ead942148
parkedhampster <jmentz@gmx.com>
2023-06-29 15:23:39 -0400

    spruce up home page make home page a welcome screen create placeholder for team information

--- Home.py
+++ Home.py
@@ -1,9 +1,26 @@
 import streamlit as st
+import pandas as pd
 
 from shared_code.user_status import side_header
 
 st.set_page_config(
-        page_title="Fantasy Pokemon League",
+        page_title="Home - Fantasy Pokemon League",
 )
 
 side_header()
+
+if 'user' in st.session_state:
+    st.title(
+        st.session_state['user'].upper()
+        )
+    st.header("Team:")
+    st.table(
+        pd.DataFrame(
+            [{
+                'Name':'UNDER CONSTRUCTION'
+            }]
+        ))
+else:
+    st.title(
+        "Please Sign in to View Personal Stats"
+    )
