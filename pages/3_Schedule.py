bb8837f78b6ca6b5d5032745405466d86e835010
parkedhampster <jmentz@gmx.com>
2023-06-30 18:24:26 -0400

    add round robin script modify schedule page to prep for updates

--- pages/3_Schedule.py
+++ pages/3_Schedule.py
@@ -1,7 +1,21 @@
 import streamlit as st
 
 from shared_code.user_status import side_header
+from shared_code.round_robin import round_robin
 
 side_header()
 
 st.markdown("# UNDER CONSTRUCTION")
+
+matches = []
+matches.append(st.expander("## Week {}"))
+matches[-1].markdown("""
+| Competitor 1 | Competitor 2 | Winner |
+| ---: | :--- | :---: |
+| Charlotte | Nick | ??? |
+| Paul      | Giuseppe | ??? |
+| Dylan     | Kaysee | ??? |
+| Joey      | Rance | ??? |
+| BYE       | Justin | Justin |
+
+""")
